a3fbcd84f79820298049ce01500421e704801f37
jedgodsey <jed.godsey@gmail.com>
2020-10-08 15:20:40 -0700

    added minimal server function

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+require('dotenv').config();
+const PORT = process.env.PORT;
+
+app.set('view engine', 'ejs');
+
+const ctrl = require('./controllers/carCtrl');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+
+
+
+app.listen(PORT, () => {
+    console.log(`This server is running on ${PORT}`)
+})
